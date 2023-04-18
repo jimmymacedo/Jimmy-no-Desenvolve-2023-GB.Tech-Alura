1d102fef37372b916841387c2ce3b23da7286faf
Jimmy Macedo <jimmymacedo@gmail.com>
2023-04-18 19:48:03 -0300

    v 0.16
    Finalizando o Curso "JavaScript: manipulando o DOM"

---
+++ robotron-2000/js/main.js
@@ -0,0 +1,67 @@
+const controle = document.querySelectorAll('[data-controle]')
+const estatisticas = document.querySelectorAll('[data-estatistica]')
+
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
+
+controle.forEach((elemento, index) => {
+    elemento.addEventListener('click', (evento) => {
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
+        atualizaEstatisticas(evento.target.dataset.peca)
+    })
+})
+
+
+function manipulaDados(operacao, controle) {
+
+    const peca = controle.querySelector('[data-contador]')
+    if (operacao === '-') {
+        peca.value = parseInt(peca.value) - 1
+    } else {
+        peca.value = parseInt(peca.value) + 1
+    }
+}
+
+function atualizaEstatisticas(peca) {
+    console.log(pecas[peca])
+
+    estatisticas.forEach( (elemento) => {
+        console.log(elemento.textContent)
+        console.log(elemento.dataset.estatistica)
+
+        elemento.textContent = 
+            parseInt(elemento.textContent) + 
+            pecas[peca][elemento.dataset.estatistica]
+    } )
+}
