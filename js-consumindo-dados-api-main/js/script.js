49d80ae30da78fa88d9d0f875e588aeef1a8ac18
Jimmy Macedo <jimmymacedo@gmail.com>
2023-04-24 10:23:23 -0300

    V 0.24
    Finalizando o Curso "JavaScript: consumindo e tratando dados de uma API", com uso da API ViaCEP, explorando conceitos de função assíncrona.

--- js-consumindo-dados-api-main/js/script.js
+++ js-consumindo-dados-api-main/js/script.js
@@ -1,1 +1,36 @@
-console.log('Hello World');
+
+async function buscaEndereco(cep) {
+    var mensagemErro = document.getElementById('erro')
+    mensagemErro.innerHTML = ""
+    try {
+        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        var consultaCEPJson = await consultaCEP.json()
+        if (consultaCEPJson.erro) {
+            throw Error("CEP inexistente")
+        } 
+        var cidade = document.getElementById('cidade')
+        var endereco = document.getElementById('endereco')
+        var estado = document.getElementById('estado')
+        var numero = document.getElementById('numero')
+        var bairro = document.getElementById('bairro')
+        
+        cidade.value = consultaCEPJson.localidade
+        endereco.value = consultaCEPJson.logradouro
+        estado.value = consultaCEPJson.uf
+        bairro.value = consultaCEPJson.bairro
+
+        numero.focus()
+
+        return consultaCEPJson
+
+    } catch (erro) {
+        mensagemErro.innerHTML = `<p> CEP Inválido, tente novamente. </p>`
+        return false
+    }
+
+    
+}
+
+var cep = document.getElementById('cep');
+cep.addEventListener('focusout', () => buscaEndereco(cep.value))
+
