4466a8dd489bd2f38b2595db20fbe0d4b73106d3
Jimmy Macedo <jimmymacedo@gmail.com>
2023-05-18 19:21:46 -0300

    v 0.37
    - mais alguns refinamentos no CLI;
    - tratamento de erro para arquivo ou diretório não localizado;
    - registro de script

--- node-lib-md/src/cli.js
+++ node-lib-md/src/cli.js
@@ -4,18 +4,31 @@ import pegaArquivo from "./index.js";
 
 const caminho = process.argv;
 
-function imprimeLista(resultado) {
-    console.log(chalk.yellow('lista de links'), resultado)
+function imprimeLista(resultado, identificador = '') {
+    console.log(
+        chalk.yellow('lista de links'), 
+        chalk.black.bgGreen(identificador),
+        resultado
+    )
 }
 
 async function processaTexto(argumentos) {
 
     const caminho = argumentos[2]
 
+    try {
+        fs.lstatSync(caminho)
+    } catch (erro) {
+        if (erro.code === 'ENOENT') {
+            console.log(chalk.red('Arquivo ou diretório não existe.'))
+            return
+        }
+    }
+
     if (fs.lstatSync(caminho).isFile() ) {
 
         const resultado = await pegaArquivo(argumentos[2])
-        imprimeLista(resultado)
+        imprimeLista(resultado, caminho)
 
     } else if (fs.lstatSync(caminho).isDirectory()) {
 
@@ -23,7 +36,7 @@ async function processaTexto(argumentos) {
 
         arquivos.forEach(async (nomeDoArquivo) => {
             const lista = await pegaArquivo(`${caminho}/${nomeDoArquivo}`)
-            imprimeLista(lista)
+            imprimeLista(lista, `${caminho}/${nomeDoArquivo}`)
         })
     }
 
