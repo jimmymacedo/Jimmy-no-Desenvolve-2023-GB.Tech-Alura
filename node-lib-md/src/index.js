296b35faa494a632bdec12299abd36834e19d8dc
Jimmy Macedo <jimmymacedo@gmail.com>
2023-05-18 16:38:19 -0300

    v 0.34
    começando implementação para uso por linha de comando

--- node-lib-md/src/index.js
+++ node-lib-md/src/index.js
@@ -1,6 +1,13 @@
 import fs from 'fs';
 import chalk from 'chalk';
 
+function extraiLinks(texto) {
+    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+    const capturas = [...texto.matchAll(regex)]
+    const resultados = capturas.map(captura => ({[captura[1]]: captura[2]}))
+    return resultados
+}
+
 function trataErro(erro) {
     throw new Error(chalk.red(erro.code, 'Arquivo não localizado.'))
 }
@@ -9,7 +16,7 @@ async function pegaArquivo(caminhoArquivo) {
     try {
         const enconding = 'utf-8'
         const texto = await fs.promises.readFile(caminhoArquivo, enconding)
-        console.log(chalk.green(texto))
+        console.log(extraiLinks(texto))
     } catch (erro) {
         trataErro(erro)
     } finally {
@@ -17,13 +24,6 @@ async function pegaArquivo(caminhoArquivo) {
     }
 }
 
-// function pegaArquivo(caminhoArquivo) {
-//     const enconding = 'utf-8'
-//     fs.promises
-//         .readFile(caminhoArquivo, enconding)
-//         .then((texto) => {console.log(chalk.green(texto))})
-//         .catch(trataErro)
-// }
-
-
 pegaArquivo('./arquivos/texto.md')
+
+export default pegaArquivo;
