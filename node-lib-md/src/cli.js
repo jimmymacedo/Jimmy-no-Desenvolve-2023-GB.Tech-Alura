737dfcffba949bb610cd6de53e1af71708d386ea
Jimmy Macedo <jimmymacedo@gmail.com>
2023-05-18 16:46:34 -0300

    v 0.35
    refatorando o uso por linha de comando.

--- node-lib-md/src/cli.js
+++ node-lib-md/src/cli.js
@@ -1,5 +1,12 @@
+import chalk from 'chalk'
 import pegaArquivo from "./index.js";
 
 const caminho = process.argv;
 
-pegaArquivo(caminho[2])
+async function processaTexto(caminho) {
+    const resultado = await pegaArquivo(caminho[2])
+    console.log(chalk.yellow('lista de links'))
+    console.log(resultado)
+}
+
+processaTexto(caminho)
--- node-lib-md/src/index.js
+++ node-lib-md/src/index.js
@@ -5,7 +5,7 @@ function extraiLinks(texto) {
     const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
     const capturas = [...texto.matchAll(regex)]
     const resultados = capturas.map(captura => ({[captura[1]]: captura[2]}))
-    return resultados
+    return resultados.length !== 0 ? resultados : 'não há links no arquivo.'
 }
 
 function trataErro(erro) {
@@ -16,11 +16,10 @@ async function pegaArquivo(caminhoArquivo) {
     try {
         const enconding = 'utf-8'
         const texto = await fs.promises.readFile(caminhoArquivo, enconding)
-        console.log(extraiLinks(texto))
+        return extraiLinks(texto)
     } catch (erro) {
         trataErro(erro)
     } finally {
-        console.log(chalk.yellow('operação concluída'));
     }
 }
 
