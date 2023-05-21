de62b7a1664c554c8ac8659e447e07c9e11fa4c2
Jimmy Macedo <jimmymacedo@gmail.com>
2023-05-21 19:50:41 -0300

    v 0.42
    Novo seders, models controladores e rotas

--- orm-sequelize/api/index.js
+++ orm-sequelize/api/index.js
@@ -1,16 +1,10 @@
 const express = require('express')
-const bodyParser = require('body-parser')
+const routes = require('./routes')
 
 const app =  express()
-
-app.use(bodyParser.json())
-
 const port = 3000
 
-app.get('/teste', (req, res) => res
-    .status(200)
-    .send({mensagem: 'boas-vindas à API'
-}))
+routes(app)
 
 app.listen(port, () => console.log(`servidor rodando na porta ${port}`))
 
