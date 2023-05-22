ee7656def711f697329feaf6d9f5a3d0460a01be
Jimmy Macedo <jimmymacedo@gmail.com>
2023-05-22 11:13:12 -0300

    v 0.45
    refatorando para usar EXPRESS

--- nodejs-api-rest-express-mongodb/server.js
+++ nodejs-api-rest-express-mongodb/server.js
@@ -1,18 +1,8 @@
-const http = require("http")
-const port = 4000
+import app from './src/app.js'
 
-const rotas = {
-    '/': 'Curso de Node',
-    '/livros': 'Entrei na página de livros',
-    '/autores': 'Listagem de Autores',
-    '/editoras': 'Listagem de Editoras'
-}
+const port = process.env.PORT || 4000
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type':'text/plain'})
-    res.end(rotas[req.url])
-})
 
-server.listen(port, () => {
+app.listen(port, () => {
     console.log(`Servidor escutando em http://localhost:${port}`)
 })
